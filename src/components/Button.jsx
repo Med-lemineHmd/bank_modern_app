db1d0bef9d5f82fb5d8381201a35227d9ff93608
Med-lemineHmd <leminlemin00@gmail.com>
2022-10-12 23:32:46 +0100

    Card Dealg Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-comfortaa font-medium text-[17px] text-primary outline-none ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient font-comfortaa font-medium rounded-[10px] text-[17px] text-primary outline-none ${styles}`}
     >
       Get Started
     </button>
